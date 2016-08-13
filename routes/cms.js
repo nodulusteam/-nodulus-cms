0583f4a9ea913dc2ef0fa0a2e58f3437bcd36c0c
roibh <roi@ewave.co.il>
2016-08-13 18:39:20 +0300

    first commit

---
+++ routes/cms.js
@@ -0,0 +1,28 @@
+﻿
+var express = require('express');
+var router = express.Router();
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+var dal = require("../app/dal.js").dal;
+ 
+ 
+
+
+
+router.get("/list", function (req, res) {
+    
+    res.json({});
+});
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+
